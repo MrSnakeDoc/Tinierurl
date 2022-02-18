a86ba2d56f7f31994b28ee167c1cae070c2d4ed9
MrSnakeDoc <selabbadi@gmail.com>
2022-02-19 00:36:58 +0100

    modified controller to test the redirect

--- app/router.js
+++ app/router.js
@@ -3,6 +3,11 @@ const { homePage, home, shorten } = require("./controllers/controller.js");
 
 const router = Router();
 
+router.use("/favicon.ico", (req, res) => {
+	res.status(204);
+	res.end();
+});
+
 router.get("/", homePage).get("/:code", home).post("/api/url/shorten", shorten);
 
 module.exports = router;
